730c6b217db9fb30adf2246a2ced7c9b00cc81aa
Akanksha Jaju <jaju.akanksha@gmail.com>
2020-09-26 03:35:26 -0700

    Wrote code for FizzBuzz condition

--- FizzBuzzProj/src/fizzbuzz/Fizzbuzz.java
+++ FizzBuzzProj/src/fizzbuzz/Fizzbuzz.java
@@ -3,13 +3,17 @@ package fizzbuzz;
 public class Fizzbuzz {
 
 	public String action(int i) {
-		if(i%3==0)
+		if(i%3==0 && i%5==0)
 		{
-			return "Fizz";
+			return "FizzBuzz";
 		}
 		else if(i%5==0)
 		{
 			return "Buzz";
+		}
+		else if(i%3==0)
+		{
+			return "Fizz";
 		}
 		return null;
 	}
